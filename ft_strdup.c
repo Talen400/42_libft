45865fdba73fb938fa19fdc9b8ef31cbcb55bb11
Talen Lavareda Pinto <TalenGitHub0@gmail.com>
2025-08-07 11:30:29 -0300

    ajust ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: tlavared <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 14:44:14 by tlavared          #+#    #+#             */
-/*   Updated: 2025/07/23 15:03:08 by tlavared         ###   ########.fr       */
+/*   Updated: 2025/08/07 11:27:34 by tlavared         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,18 @@ char	*ft_strdup(const char *s)
 {
 	char	*str;
 	char	*ptr;
+	size_t	i;
 
 	ptr = (char *) s;
 	str = malloc((ft_strlen(ptr) + 1) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	ft_strlcpy(str, ptr, ft_strlen(ptr) + 1);
+	i = 0;
+	while (ptr[i] != '\0')
+	{
+		str[i] = ptr[i];
+		i++;
+	}
+	str[i] = '\0';
 	return (str);
 }
